5f302aadc35a4c0d7fb5e4be33a2cb448ec545cc
kasia-chodak <kasiachodak4444@gmail.com>
2024-04-15 23:43:05 +0200

    Make PageFooter and PageTopPart work

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -7,21 +7,25 @@
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Gluten:wght@100..900&display=swap');
+
+* {
+  margin: 0;
+  border: 0;
+}
 
 footer {
   position: sticky;
+  left: 0;
   bottom: 0;
   width: 100%;
-  display: flex;
-  flex-flow: row wrap;
+  background-color: #7da193;
+  text-align: center;
+  font-family: Gluten, "Courier New", sans-serif;
   padding: 5px;
-  color: #2f2f2f;
-  background-color: #7DA193;
-  margin: auto;
+  flex-shrink: 0;
+  z-index: 1000;
 }
 
-footer > * {
-  flex:  1 100%;
-}
 
 </style>
