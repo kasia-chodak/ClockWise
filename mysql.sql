0cd214d052a3f581e52aa2e372e688ee6e3407e6
duda-kacper <kacper.duda@10g.pl>
2024-05-09 18:20:41 +0200

    09.05

---
+++ mysql.sql
@@ -0,0 +1,8 @@
+--CREATE TABLE Users (
+    usr_id INTEGER NOT NULL  PRIMARY KEY,
+    usr_name VARCHAR NOT NULL,
+    usr_password VARCHAR NOT NULL
+);
+
+INSERT INTO Users(usr_id,usr_name,usr_password)
+VALUES(1,Kacper,123)
