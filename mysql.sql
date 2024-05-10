7e10b01328ee6b68e63ecd85c8eeb0f05a2a1fc8
duda-kacper <kacper.duda@10g.pl>
2024-05-10 12:13:46 +0200

    10.05 2

--- mysql.sql
+++ mysql.sql
@@ -1,4 +1,4 @@
---CREATE TABLE Users (
+/*CREATE TABLE Users (
     usr_id INTEGER NOT NULL  PRIMARY KEY,
     usr_name VARCHAR NOT NULL,
     usr_password VARCHAR NOT NULL
@@ -6,3 +6,8 @@
 
 INSERT INTO Users(usr_id,usr_name,usr_password)
 VALUES(1,Kacper,123)
+*/
+CREAT TABLE Tasks
+    tsk_id INTEGER NOT NULL PRIMARY KEY,
+    tsk_name VARCHAR NOT NULL,
+    
