c77add6043cfb49ec1e8c5e9dfae59adafe46dcb
duda-kacper <kacper.duda@10g.pl>
2024-05-13 20:51:31 +0200

    aktualizacja bazy danych

--- mysql.sql
+++ mysql.sql
@@ -6,8 +6,14 @@
 
 INSERT INTO Users(usr_id,usr_name,usr_password)
 VALUES(1,Kacper,123)
-*/
+
 CREATE TABLE Tasks(
     tsk_id INTEGER NOT NULL PRIMARY KEY,
     tsk_name VARCHAR NOT NULL,
     tsk_execution_date DATETIME NOT NULL)
+    */
+--DELETE FROM Tasks
+--ALTER TABLE Tasks
+--ADD COLUMN tsk_usrid INTEGER NOT NULL
+
+ADD CONSTRAINT fk_user FOREIGN KEY (tsk_usrid) REFERENCES Users (usr_id)
