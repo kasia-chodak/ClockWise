4c5fc8db2e281002c383d30c78560c1ebccf9c5f
ada0345 <ada_niemiec@yahoo.com>
2024-05-14 01:00:50 +0200

    new

---
+++ src/views/TimerviewPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <page-top-part/>
+      <main-component-timerview/>
+      <page-footer/>
+    </div>
+  </template>
+  
+  <script>
+  import PageTopPart from "@/components/PageTopPart.vue";
+  import MainComponentTimerview from "@/components/MainComponentTimerview.vue"
+  import PageFooter from "@/components/PageFooter.vue";
+  
+  export default {
+    components: {
+      PageTopPart,
+      MainComponentTimerview,
+      PageFooter,
+    }
+  }
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
